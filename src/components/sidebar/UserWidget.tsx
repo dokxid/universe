507937607621ab89d91eea42fc5bdac21097d011
dokxid <24466519+dokxid@users.noreply.github.com>
2025-09-15 21:52:35 +0200

    style userwidget when logged in

--- src/components/sidebar/UserWidget.tsx
+++ src/components/sidebar/UserWidget.tsx
@@ -3,6 +3,8 @@ import {signOutAction} from "@/actions/auth";
 import Link from "next/link";
 import {getSignInUrl, getSignUpUrl} from "@workos-inc/authkit-nextjs";
 import {useEffect, useState} from "react";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Settings} from "lucide-react";
 
 
 export function UserWidget() {
@@ -38,12 +40,22 @@ export function UserWidget() {
 
     return (
         <>
-            <p>Welcome back{user?.firstName && `, ${user?.firstName}`}</p>
-            <form
-                action={signOutAction}
-            >
-                <button type="submit">Sign out</button>
-            </form>
+            <div className={"flex flex-row items-center gap-2 bg-primary-foreground text-primary p-2 rounded-md"}>
+                <Avatar>
+                    <AvatarImage></AvatarImage>
+                    <AvatarFallback>
+                        {(user.firstName && user.lastName) ? user.firstName[0] + user.lastName[0] : ""}
+                    </AvatarFallback>
+                </Avatar>
+                <div className={"text-sm flex flex-col grow"}>
+                    <p className={"font-semibold"}>{`${user?.firstName} ${user?.lastName}`}</p>
+                    <form action={signOutAction}>
+                        <button type="submit" className={"text-xs hover:cursor-pointer hover:underline"}>Sign out
+                        </button>
+                    </form>
+                </div>
+                <Settings strokeWidth={2} className={"size-5"}/>
+            </div>
         </>
     );
 }
