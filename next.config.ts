3130bcc7eeade571cc3df46593b90507b4f9e6d5
dokxid <24466519+dokxid@users.noreply.github.com>
2025-09-18 20:31:16 +0200

    fixed project root for node.js interpretter

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    // added because my node activated from ~/ instead of directory root
+    outputFileTracingRoot: path.join(__dirname),
+
     images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "heritagelab.center",
-        }, {
-            protocol: "https",
-            hostname: "hl-universe-staging.s3.eu-central-1.amazonaws.com",
-        }]
+        remotePatterns: [
+            // images taken from the heritagelab.center site
+            {
+                protocol: "https",
+                hostname: "heritagelab.center",
+            },
+            // images from s3 bucket
+            {
+                protocol: "https",
+                hostname: "hl-universe-staging.s3.eu-central-1.amazonaws.com",
+            },
+        ],
     },
 };
 
