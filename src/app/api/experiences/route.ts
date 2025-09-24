f60872c1b6c4f65cb8d5f81e1324041f8baeb174
dokxid <24466519+dokxid@users.noreply.github.com>
2025-09-24 17:27:46 +0200

    refactor: remove unused api routes

--- src/app/api/experiences/route.ts
+++
@@ -1,6 +0,0 @@
-import { getExperiencesDTO } from "@/data/dto/experience-dto";
-
-export async function GET() {
-    const experiences = await getExperiencesDTO();
-    return Response.json(experiences);
-}
--- src/app/api/stories/route.ts
+++
@@ -1,6 +0,0 @@
-import { getAllPublicStoriesDTO } from "@/data/dto/story-dto";
-
-export async function GET() {
-    const stories = await getAllPublicStoriesDTO();
-    return Response.json(stories);
-}
