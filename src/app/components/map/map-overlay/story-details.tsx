195af0be9cc71faa75bcac3a96d39ac8123a8119
dokxid <24466519+dokxid@users.noreply.github.com>
2025-09-25 21:11:51 +0200

    style: StoryDetailsHeader  improve layout

--- src/app/components/map/map-overlay/story-details.tsx
+++ src/app/components/map/map-overlay/story-details.tsx
@@ -4,34 +4,43 @@ import S3Image from "@/app/components/s3-image";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Drawer, DrawerContent, DrawerHeader } from "@/components/ui/drawer";
-import { Separator } from "@/components/ui/separator";
+import { Drawer, DrawerContent } from "@/components/ui/drawer";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { useAppSelector } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
 import { StoryDTO } from "@/types/api";
 import parse from "html-react-parser";
 import Link from "next/link";
 import { useMemo } from "react";
 
-function StoryDetailsHeader({ author_name, title, experience, _id }: StoryDTO) {
+function StoryDetailsHeader({
+    story,
+    className,
+}: {
+    story: StoryDTO;
+    className?: string;
+}) {
     return (
         <div
-            className={
-                "flex flex-row items-center mb-2 justify-between sticky py-3 top-0 bg-card"
-            }
+            className={cn(
+                "flex flex-row items-center mb-2 justify-between sticky py-3 top-0",
+                className
+            )}
         >
             <div className={"flex flex-row items-center gap-3"}>
                 <Avatar>
-                    <AvatarFallback>{author_name.charAt(0)}</AvatarFallback>
+                    <AvatarFallback>
+                        {story.author_name.charAt(0)}
+                    </AvatarFallback>
                 </Avatar>
                 <article className={"flex flex-col text-left"}>
-                    <p className={"font-semibold"}>{title}</p>
+                    <p className={"font-semibold"}>{story.title}</p>
                     <p className={"text-sm text-muted-foreground"}>
-                        by {author_name}
+                        by {story.author_name}
                     </p>
                 </article>
             </div>
-            <Link href={"/" + experience + "/stories/" + _id}>
+            <Link href={"/" + story.experience + "/stories/" + story._id}>
                 <Button variant={"link"} className={"p-0"}>
                     Read more
                 </Button>
@@ -65,11 +74,11 @@ export function StoryDetails({
         return (
             <Drawer open={open} onOpenChange={setOpenAction}>
                 <DrawerContent>
-                    <div className={"p-1 overflow-y-auto flex-1"}>
-                        <DrawerHeader className={"gap-4"}>
-                            <StoryDetailsHeader {...story} />
-                        </DrawerHeader>
-                        <Separator />
+                    <div className={"px-6 pb-6 overflow-y-auto flex-1"}>
+                        <StoryDetailsHeader
+                            story={story}
+                            className={"bg-background"}
+                        />
                         <div className="prose dark:prose-invert px-4">
                             {parse(story.content)}
                         </div>
@@ -93,7 +102,7 @@ export function StoryDetails({
                 />
             </div>
             <div className={"px-6 pb-6 flex flex-col"}>
-                <StoryDetailsHeader {...story} />
+                <StoryDetailsHeader story={story} className={"bg-card"} />
                 <div className="prose dark:prose-invert prose-headings:mb-2">
                     {parse(story.content)}
                 </div>
