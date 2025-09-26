f4be219fd2379ea40841cbef3b4bfbea92cbb024
dokxid <24466519+dokxid@users.noreply.github.com>
2025-09-26 15:06:36 +0200

    feat: add @vercel/speed-insights for monitoring responsiveness and loading speeds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/app/components/providers/theme-provider";
 import { SidebarLayout } from "@/app/components/sidebar/sidebar-wrapper";
 import StoreProvider from "@/app/store-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AuthKitProvider } from "@workos-inc/authkit-nextjs/components";
 import type { Metadata } from "next";
 import React from "react";
@@ -17,7 +18,7 @@ export const metadata: Metadata = {
     title: "Heritage Lab Universe",
     description: "Explore cultures, their history and stories",
 };
-export default function RootLayout({ children, modal }: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
@@ -35,11 +36,11 @@ export default function RootLayout({ children, modal }: RootLayoutProps) {
                                     disableTransitionOnChange
                                 >
                                     <SidebarLayout>
-                                        <div>{modal}</div>
                                         <div className="flex grow">
                                             {children}
                                         </div>
                                         <Toaster />
+                                        <SpeedInsights />
                                     </SidebarLayout>
                                 </ThemeProvider>
                             </main>
