08c76e99d57ac4cf98cbf2201a3226283615ed53
dokxid <dan-du@live.de>
2025-10-10 19:24:25 +0200

    feat: fix vercel deployment error https://nextjs.org/docs/messages/middleware-relative-urls

--- src/app/favicon.ico/route.ts
+++ src/app/favicon.ico/route.ts
@@ -1,7 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // sometimes browser forces a request to /favicon.ico even tho its defined differently in the layout
 // this is a workaround to redirect to the correct location
-export function GET() {
-    return NextResponse.redirect("/img/favicon.ico", 307);
+export function GET(request: NextRequest) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/img/favicon.ico";
+    return NextResponse.redirect(url, 307);
 }
