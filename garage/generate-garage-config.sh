54cd372e5389c1519caaab5f03c94826ea111288
dokxid <dan-du@live.de>
2025-10-20 16:29:31 +0200

    feat: merge garage configs to main repo

---
+++ garage/generate-garage-config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Load environment variables from .env file
+[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
+
+cat > garage.toml <<EOF
+metadata_dir = "/tmp/meta"
+data_dir = "/tmp/data"
+db_engine = "sqlite"
+
+replication_factor = 1
+
+rpc_bind_addr = "[::]:${PORT_RPC}"
+rpc_public_addr = "127.0.0.1:${PORT_RPC}"
+rpc_secret = "$(openssl rand -hex 32)"
+
+[s3_api]
+s3_region = "garage"
+api_bind_addr = "[::]:${PORT_S3_API}"
+root_domain = ".s3.garage.localhost"
+
+[s3_web]
+bind_addr = "[::]:${PORT_S3_WEB}"
+root_domain = ".web.garage.localhost"
+index = "index.html"
+
+[k2v_api]
+api_bind_addr = "[::]:${PORT_KV2_API}"
+
+[admin]
+api_bind_addr = "[::]:${PORT_ADMIN}"
+admin_token = "$(openssl rand -base64 32)"
+metrics_token = "$(openssl rand -base64 32)"
+EOF
---
+++ garage/setup-garage.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# Load environment variables from .env file
+[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
+
+set -x
+
+garage="docker exec -ti garage-universe-garage-1 /garage"
+
+$garage status
+
+# Get node ID from garage status output
+GARAGE_NODE_ID=$($garage status | grep -E "^[a-f0-9]{16}" | awk '{print $1}')
+echo "Detected Garage Node ID: $GARAGE_NODE_ID"
+
+$garage layout assign "${GARAGE_NODE_ID}" -z local -c "${GARAGE_SIZE}" -t "${GARAGE_NODE_NAME}"
+$garage layout apply --version 1
+
+$garage bucket create universe
+$garage key create next_server | tee keys/.next_server
+$garage bucket allow \
+  --read \
+  --write \
+  --owner \
+  universe \
+  --key next_server
+$garage key create aws_cli | tee keys/.aws_cli
+$garage bucket allow \
+  --read \
+  --write \
+  --owner \
+  universe \
+  --key aws_cli
