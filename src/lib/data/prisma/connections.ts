7dff237cbc290bc3e55aee5a0832e8ffd5ffd733
dokxid <dan-du@live.de>
2025-11-09 13:31:05 +0100

    tidy: remove unused packages, add connectionstring print for debug

--- src/lib/data/prisma/connections.ts
+++ src/lib/data/prisma/connections.ts
@@ -1,21 +1,22 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-const adapter = new PrismaPg({
-    connectionString: `
+const connectionString = `
     postgresql://
     ${process.env.POSTGRES_USER}:
     ${process.env.POSTGRES_PASSWORD}@
     ${process.env.POSTGRES_HOST}/
     ${process.env.POSTGRES_DB}
-`})
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
-// const connectionString = `${process.env.DATABASE_URL}`;
-// const adapter = new PrismaPg({ connectionString });
-// export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+`
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
+console.log("Postgres connection string: ", connectionString)
+
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const adapter = new PrismaPg({
+    connectionString
+})
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
